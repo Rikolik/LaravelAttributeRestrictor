7bf9f5174cd0db2dcac004a0b26eeeee8e47e87b
Rikolik <henriquelc000@gmail.com>
2025-01-25 13:46:41 -0300

    Fixing service provider... again

---
+++ src/LaravelAttributeRestrictorServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Enriko\LaravelAttributeRestrictor;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAttributeRestrictorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/restriction.php',
+            'restriction' // Config key
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/restriction.php' => config_path('restriction.php'),
+        ], 'config');
+    }
+}
